77675ac272c6f9c12fed3a54b02ad9354556d08e
Medido1 <medi1222@gmail.com>
2025-06-18 08:20:32 +0100

    Add a new comment logic impelementd

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,11 +1,60 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import userAvatar from "/avatars/image-juliusomo.png"
+import {GlobalContext} from "../context/GlobalContext";
 
 function CommentForm() {
   const [commentText, setCommentText] = useState("");
+  const {data, setData} = useContext(GlobalContext)
 
+  function getNextId(data) { /* get the id of the new comment */
+    if (!data.comments || data.comments.length === 0) return 1;
+    let lastId = - Infinity;
+    
+    function traverseComments(comments) {
+      for (let comment of comments) {
+        if (comment.id > lastId) {
+          lastId = comment.id
+        }
+        if (comment.replies) {
+          traverseComments(comment.replies)
+        }
+      }
+    }
+
+    traverseComments(data.comments)
+    return lastId + 1;
+  }
+
+  function submitComment(e) {
+    e.preventDefault();
+    if (!commentText.trim()) {
+      alert("Write something damn it!!")
+      return;
+    }
+
+    const newComment = {
+      id: getNextId(data),
+      content : commentText,
+      createdAt: "just now",
+      score: 0,
+      user: {
+        "image": { 
+          "png": "/avatars/image-juliusomo.png",
+          "webp": "/avatars/image-juliusomo.webp"
+        },
+        "username": "juliusomo"
+      },
+      replies: []
+    }
+
+    const updatedComments = [...data.comments, newComment];
+    setData(prev => ({...prev, comments: updatedComments}))
+    setCommentText("")
+  }
   return (
-    <form className="bg-white p-4">
+    <form 
+      onSubmit={submitComment}
+      className="bg-white p-4">
       <label htmlFor="commentText" className="sr-only">Add a comment</label>
       <textarea 
         className="border-gray-200 border p-4 rounded-lg mb-4" 
@@ -15,6 +64,7 @@ function CommentForm() {
         placeholder="Add a comment..."
         value = {commentText}
         onChange={(e) => setCommentText(e.target.value)}
+        aria-label="Add a comment"
       ></textarea>
       <div className="flex justify-between items-center">
         <img 
