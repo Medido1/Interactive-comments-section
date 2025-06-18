8d84ef9f29dadf5bdff338c0cc640322a5188f04
Medido1 <medi1222@gmail.com>
2025-06-18 09:38:36 +0100

    Use useMemo on sorting to avoid sorting at every reender

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { GlobalContext } from "./context/GlobalContext";
 import CommentComp from "./components/CommentComp";
 import CommentForm from "./components/CommentForm";
@@ -7,7 +7,10 @@ import CommentForm from "./components/CommentForm";
 function App() {
   const { data } = useContext(GlobalContext);
 
-  const sortedComments = data.comments ? [...data.comments].sort((a, b) => b.score - a.score): []
+   /* only sort when data is modified insted of sorting at every render */
+  const sortedComments = useMemo(() => {
+    return data.comments ? [...data.comments].sort((a, b) => b.score - a.score): []
+  }, [data.comments])
   return (
     <main className="bg-gray-100 min-h-screen px-4 py-6 flex justify-center">
       <div>
